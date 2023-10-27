1e6877c7df3baf8fa606a8d39105547177af3a6b
pault05 <paul_trif03@yahoo.com>
2023-10-27 10:25:28 +0300

    update 3

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,8 @@
 public class Client {
+
+    private String numeClient;
+    public String getNumeClient() { return numeClient; }
+
+    public masinaClient = new Masina;
+
 }
--- src/Masina.java
+++ src/Masina.java
@@ -1,2 +1,25 @@
 public class Masina {
+
+    private String seria;
+    public String getSeria() { return seria; }
+
+    private String marca;
+    public String getMarca() { return marca; }
+
+    private int an;
+    public int getAn() { return an; }
+
+    private String tip;
+    public String getTip() { return tip; }
+
+    public Masina(String seria, String marca, int an, String tip)
+    {
+        this.marca=marca;
+        this.seria=seria;
+        this.an=an;
+        this.tip=tip;
+    }
+
+    public String toString() { return seria + " " + marca + " " + an + " " + tip; }
+
 }
