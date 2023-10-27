801cec340a7a0ad090a011bfa3cca0994a7ec518
pault05 <paul_trif03@yahoo.com>
2023-10-27 10:25:28 +0300

    update 4 : Masina

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,21 @@
 public class Client {
+
+    private String numeClient;
+    public String getNumeClient() { return numeClient; }
+    public void setNumeClient(String numeClient){ this.numeClient=numeClient; }
+
+    private String telefonClient;
+    public String getTelefonClient() { return telefonClient; }
+    public void setTelefonClient(String telefonClient) { this.telefonClient = telefonClient; }
+
+    private Masina masinaClient;
+    public Masina getMasinaClient() { return masinaClient; }
+    public void setMasinaClient(Masina masinaClient) { this.masinaClient = masinaClient; }
+
+
+    public Client(String numeClient, String telefonClient, Masina masinaClient){
+        this.masinaClient=masinaClient;
+        this.telefonClient=telefonClient;
+        this.numeClient=numeClient;
+    }
 }
